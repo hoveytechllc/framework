a14141639b8b8cc7ae4eebab8203cace3068d928
David Hovey <david@hoveytech.com>
2017-03-02 17:37:00 -0700

    Changed property types to xproj/project.json

--- src/HoveyTech.Data.EfCore/Properties/AssemblyInfo.cs
+++ src/HoveyTech.Data.EfCore/Properties/AssemblyInfo.cs
@@ -1,3 +1,19 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("HoveyTech.Data.EfCore")]
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HoveyTech.Data.EfCore")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8e6296ee-97b8-4537-a50b-d542b5f3272a")]
