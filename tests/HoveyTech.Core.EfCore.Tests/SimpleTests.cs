fc9ae6056a4b9a9f4021f80246303f179c0daa32
David Hovey <david@hoveytech.com>
2017-03-07 15:35:14 -0700

    Converting projects to VS2017 type

---
+++ tests/HoveyTech.Core.EfCore.Tests/SimpleTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace HoveyTech.Core.EfCore.Tests
+{
+    public class SimpleTests
+    {
+        [Fact]
+        public void Sanity_test()
+        {
+            Assert.True(true);
+        }
+    }
+}
