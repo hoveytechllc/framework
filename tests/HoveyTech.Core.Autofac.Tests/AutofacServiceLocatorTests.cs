33037f3c4361d9eb199923fab0d4b8773a946c11
David Hovey <david@hoveytech.com>
2018-05-10 13:11:22 -0700

    Added autofac tests

---
+++ tests/HoveyTech.Core.Autofac.Tests/AutofacServiceLocatorTests.cs
@@ -0,0 +1,25 @@
+﻿using Autofac;
+using Xunit;
+
+namespace HoveyTech.Core.Autofac.Tests
+{
+    public class AutofacServiceLocatorTests
+    {
+        public class TestRegisteredObject
+        {
+
+        }
+
+        [Fact]
+        public void Resolve_does_return_instance_of_registration()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<TestRegisteredObject>();
+
+            var sut = new AutofacServiceContainer(builder.Build());
+            
+            TestRegisteredObject result = sut.Resolve<TestRegisteredObject>();
+            Assert.NotNull(result);
+        }
+    }
+}
---
+++ tests/HoveyTech.Core.Autofac.Tests/LazyAutofacServiceLocatorTests.cs
@@ -0,0 +1,47 @@
+﻿using Autofac;
+using HoveyTech.Core.Contracts;
+using HoveyTech.Core.Services;
+using Xunit;
+
+namespace HoveyTech.Core.Autofac.Tests
+{
+    public class TestableLazyAutofacServiceLocator : LazyAutofacServiceContainer
+    {
+        public int BuildCount { get; private set; }
+
+        protected override void SetupContainerBuilder(ContainerBuilder builder)
+        {
+            BuildCount++;
+            builder.RegisterType<DateTimeFactory>().AsImplementedInterfaces();
+        }
+    }
+
+    public class LazyAutofacServiceLocatorTests
+    {
+        [Fact]
+        public void Resolve_does_build_once_if_called_multiple_times()
+        {
+            var sut = new TestableLazyAutofacServiceLocator();
+
+            sut.Resolve<IDateTimeFactory>();
+            sut.Resolve<IDateTimeFactory>();
+            sut.Resolve<IDateTimeFactory>();
+
+            Assert.Equal(1, sut.BuildCount);
+        }
+        
+        [Fact]
+        public void Container_does_build_container()
+        {
+            var sut = new TestableLazyAutofacServiceLocator();
+
+            var container1 = sut.Container;
+            var container2 = sut.Container;
+            var container3 = sut.Container;
+            
+            Assert.Equal(1, sut.BuildCount);
+            Assert.Equal(container1, container2);
+            Assert.Equal(container2, container3);
+        }
+    }
+}
